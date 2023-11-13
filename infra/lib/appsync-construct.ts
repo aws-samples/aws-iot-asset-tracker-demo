d9589392038796c35eb92dfeb86430931c85d679
Zach Elliott <awszac@amazon.com>
2023-11-13 11:18:31 -0600

    Syncing between Github

---
+++ infra/lib/appsync-construct.ts
@@ -0,0 +1,109 @@
+import { StackProps, Expiration, CfnOutput, Duration } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  GraphqlApi,
+  SchemaFile,
+  AuthorizationType,
+  MappingTemplate,
+} from "aws-cdk-lib/aws-appsync";
+import { NagSuppressions } from "cdk-nag";
+import { Function } from "aws-cdk-lib/aws-lambda";
+
+interface AppSyncConstructProps extends StackProps {
+  lambdaFnResolver: Function;
+}
+
+export class AppSyncConstruct extends Construct {
+  api: GraphqlApi;
+
+  constructor(scope: Construct, id: string, props: AppSyncConstructProps) {
+    super(scope, id);
+
+    const { lambdaFnResolver } = props;
+
+    this.api = new GraphqlApi(this, "Api", {
+      name: "AssetTracker",
+      schema: SchemaFile.fromAsset("./lib/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: Expiration.after(Duration.days(365)),
+          },
+        },
+        additionalAuthorizationModes: [
+          {
+            authorizationType: AuthorizationType.IAM,
+          },
+        ],
+      },
+    });
+
+    NagSuppressions.addResourceSuppressions(this.api, [
+      {
+        id: "AwsSolutions-ASC3",
+        reason:
+          "This API is deployed as part of an AWS workshop and as such it's short-lived. Analyzing the logs is not part of the workshop.",
+      },
+    ]);
+
+    const noneSource = this.api.addNoneDataSource("NoneSource");
+
+    noneSource.createResolver("update-position-resolver", {
+      typeName: "Mutation",
+      fieldName: "updatePosition",
+      requestMappingTemplate: MappingTemplate.fromString(`{
+        "version": "2018-05-29",
+        "payload": $util.toJson($context.arguments)
+    }`),
+      responseMappingTemplate: MappingTemplate.fromString(
+        `$util.toJson($context.result.input)`
+      ),
+    });
+
+    noneSource.createResolver("send-geofence-event-resolver", {
+      typeName: "Mutation",
+      fieldName: "sendGeofenceEvent",
+      requestMappingTemplate: MappingTemplate.fromString(`{
+        "version": "2018-05-29",
+        "payload": $util.toJson($context.arguments)
+    }`),
+      responseMappingTemplate: MappingTemplate.fromString(
+        `$util.toJson($context.result.input)`
+      ),
+    });
+
+    const lambdaSource = this.api.addLambdaDataSource(
+      "LambdaSource",
+      lambdaFnResolver
+    );
+
+    lambdaSource.createResolver("get-device-history-resolver", {
+      typeName: "Query",
+      fieldName: "getDeviceHistory",
+      requestMappingTemplate: MappingTemplate.fromString(`{
+        "version": "2018-05-29",
+        "operation": "Invoke",
+        "payload": $util.toJson($context)
+    }`),
+      responseMappingTemplate: MappingTemplate.fromString(
+        `#if($ctx.error)
+        $util.error($ctx.error.message, $ctx.error.type)
+      #end
+      $util.toJson($ctx.result)`
+      ),
+    });
+
+    new CfnOutput(this, "ApiUrl", {
+      value: this.api.graphqlUrl,
+    });
+
+    new CfnOutput(this, "ApiId", {
+      value: this.api.apiId,
+    });
+
+    new CfnOutput(this, "ApiKey", {
+      value: this.api.apiKey as string,
+    });
+  }
+}
---
+++ infra/lib/auth-construct.ts
@@ -0,0 +1,70 @@
+import { Stack, StackProps, CfnOutput } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { IRole, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { IdentityPool } from "@aws-cdk/aws-cognito-identitypool-alpha";
+import { NagSuppressions } from "cdk-nag";
+
+interface AuthConstructProps extends StackProps { }
+
+export class AuthConstruct extends Construct {
+  unauthRole: IRole;
+
+  constructor(scope: Construct, id: string, _props: AuthConstructProps) {
+    super(scope, id);
+
+    const identityPool = new IdentityPool(this, "IdentityPool", {
+      allowUnauthenticatedIdentities: true,
+    });
+    const { unauthenticatedRole, identityPoolId } = identityPool;
+
+    NagSuppressions.addResourceSuppressions(identityPool, [
+      {
+        id: "AwsSolutions-COG7",
+        reason:
+          "Application uses unauthenticated access (i.e. guest) only, so this setting is needed.",
+      },
+    ]);
+
+    this.unauthRole = unauthenticatedRole;
+    this.unauthRole.attachInlinePolicy(
+      new Policy(this, "locationService", {
+        statements: [
+          new PolicyStatement({
+            actions: [
+              "geo:GetMapGlyphs",
+              "geo:GetMapSprites",
+              "geo:GetMapStyleDescriptor",
+              "geo:GetMapTile",
+            ],
+            resources: [
+              `arn:aws:geo:${Stack.of(this).region}:${Stack.of(this).account
+              }:map/AssetTrackerMap`,
+            ],
+          }),
+          new PolicyStatement({
+            actions: [
+              "geo:ListGeofences",
+              "geo:BatchPutGeofence",
+              "geo:BatchDeleteGeofence",
+            ],
+            resources: [
+              `arn:aws:geo:${Stack.of(this).region}:${Stack.of(this).account
+              }:geofence-collection/AssetTrackerGeofenceCollection`,
+            ],
+          }),
+          new PolicyStatement({
+            actions: ["geo:CalculateRoute"],
+            resources: [
+              `arn:aws:geo:${Stack.of(this).region}:${Stack.of(this).account
+              }:route-calculator/AssetTrackerRouteCalculator`,
+            ],
+          }),
+        ],
+      })
+    );
+
+    new CfnOutput(this, "IdentityPoolId", {
+      value: identityPoolId,
+    });
+  }
+}
